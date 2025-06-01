2287bbd3f6c0b065ee33c5b369358326ed1b7a90
Büşra Akay <61781887+busraakay@users.noreply.github.com>
2025-06-01 16:31:07 +0300

    IdentityService added.

--- src/Services/IdentityService/IdentityService.Api/obj/Debug/net6.0/IdentityService.Api.AssemblyInfo.cs
+++ src/Services/IdentityService/IdentityService.Api/obj/Debug/net6.0/IdentityService.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityService.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+587b9d09c831179bdf3932d462d1739fbc7394b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+138c19a8384cba5fa027f0d0f8587a6c32a0cea7")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityService.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityService.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
